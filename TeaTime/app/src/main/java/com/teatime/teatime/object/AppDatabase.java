2b53fa27671815672215f42d7f35fb8126a58a1d
kbelisle <belisle57@msn.com>
2018-04-15 19:58:09 -0400

    Changement Version Final

--- TeaTime/app/src/main/java/com/teatime/teatime/object/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.teatime.teatime.object;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {Tea.class}, version = 5, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract TeaDao teaDao();
-}
